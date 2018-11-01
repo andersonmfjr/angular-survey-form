f2fbcd813fdcb987401823301fff03882a0dd32b
Anderson Júnior <andersonmfjr@gmail.com>
2018-11-01 20:14:15 -0300

    Subindo algumas perguntas-exemplo

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -14,42 +14,24 @@ export class QuestionService {
       resolve(this.getQuestionsOriginal());
     });
   }*/
-  getQuestions() {
-    const questions: QuestionBase<any>[] = [
-      new DropdownQuestion({
-        key: 'rating',
-        label: 'Bravery Rating',
-        value: '',
-        options: [
-          { key: 'solid', value: 'Solid' },
-          { key: 'great', value: 'Great' },
-          { key: 'good', value: 'Good' },
-          { key: 'unproven', value: 'Unproven' }
-        ],
-        divId: 'questao3',
-        order: 3
-      }),
 
-      new RadioQuestion({
-        key: 'color',
-        label: 'Radio question',
-        required: true,
-        options: {
-          firstcolumn: [
-            { key: 'radio1', name: 'color', value: 'red', label: 'Red' },
-            { key: 'radio2', name: 'color', value: 'green', label: 'Green' },
-            { key: 'radio3', name: 'color', value: 'blue', label: 'Blue' }
-          ],
-          secondcolumn: [
-            { key: 'radio4', name: 'color', value: 'yellow', label: 'Yellow' },
-            { key: 'radio5', name: 'color', value: 'white', label: 'White' }
-          ]
-        },
-        divId: 'questao1',
-        order: 1
-      }),
+  /*
+  new DropdownQuestion({
+    key: 'rating',
+    label: 'Bravery Rating',
+    value: '',
+    options: [
+      { key: 'solid', value: 'Solid' },
+      { key: 'great', value: 'Great' },
+      { key: 'good', value: 'Good' },
+      { key: 'unproven', value: 'Unproven' }
+    ],
+    order: 3
+  })
+  */
 
-      new TextareaQuestion({
+  /*
+  new TextareaQuestion({
         key: 'name',
         label: 'First name',
         value: '',
@@ -57,13 +39,141 @@ export class QuestionService {
         divId: 'questao4',
         order: 4
       }),
+*/
+
+  getQuestions() {
+    const questions: QuestionBase<any>[] = [
+      new DropdownQuestion({
+        key: 'grade',
+        label: 'Selecione a sua grade curricular:',
+        value: '',
+        required: true,
+        options: [
+          { key: '2018', value: '2018 - Grade nova' },
+          { key: '2012', value: '2012 - Grade nova' }
+        ],
+        order: 1
+      }),
+
+      new DropdownQuestion({
+        key: 'materia',
+        label: 'Selecione a matéria:',
+        value: '',
+        required: true,
+        options: [
+          { key: 'orge', value: 'Organização de empresas' },
+          { key: 'proo', value: 'Programação orientada a objetos' },
+          { key: 'mpec', value: 'Metodologia da pesquisa científica' }
+        ],
+        order: 2
+      }),
+
+      new DropdownQuestion({
+        key: 'materia',
+        label: 'Selecione o professor:',
+        value: '',
+        required: true,
+        options: [
+          { key: 'flavio', value: 'Flávio Mota Medeiros' },
+          { key: 'elvys', value: 'Elvys' }
+        ],
+        order: 3
+      }),
+
+      new RadioQuestion({
+        key: 'dominioconteudo',
+        label: 'O professor aparenta ter domínio do conteúdo?',
+        required: true,
+        options: {
+          firstcolumn: [
+            {
+              key: 'radio1',
+              name: 'dominioconteudo',
+              value: '5',
+              label: 'Extremamente'
+            },
+            {
+              key: 'radio2',
+              name: 'dominioconteudo',
+              value: '4',
+              label: 'Muito'
+            },
+            {
+              key: 'radio3',
+              name: 'dominioconteudo',
+              value: '3',
+              label: 'Um pouco'
+            }
+          ],
+          secondcolumn: [
+            {
+              key: 'radio4',
+              name: 'dominioconteudo',
+              value: '2',
+              label: 'Nem tanto'
+            },
+            {
+              key: 'radio5',
+              name: 'dominioconteudo',
+              value: '1',
+              label: 'De forma alguma'
+            }
+          ]
+        },
+        order: 4
+      }),
+
+      new RadioQuestion({
+        key: 'conteudoclaro',
+        label:
+          'O quão claro o professor apresentou o material de curso (método de avaliação, cronograma, etc)?',
+        required: true,
+        options: {
+          firstcolumn: [
+            {
+              key: 'radio1',
+              name: 'conteudoclaro',
+              value: '5',
+              label: 'Extremamente claro'
+            },
+            {
+              key: 'radio2',
+              name: 'conteudoclaro',
+              value: '4',
+              label: 'Muito claro'
+            },
+            {
+              key: 'radio3',
+              name: 'conteudoclaro',
+              value: '3',
+              label: 'Um pouco'
+            }
+          ],
+          secondcolumn: [
+            {
+              key: 'radio4',
+              name: 'conteudoclaro',
+              value: '2',
+              label: 'Nem tanto'
+            },
+            {
+              key: 'radio5',
+              name: 'conteudoclaro',
+              value: '1',
+              label: 'De forma alguma'
+            }
+          ]
+        },
+        order: 5
+      }),
 
       new TextareaQuestion({
-        key: 'email',
-        label: 'Email',
+        key: 'profaberta',
+        label:
+          'Tem algum outro comentário a acrescentar sobre o professor ou sobre a matéria? Críticas, comentários, dicas?',
         value: '',
-        divId: 'questao2',
-        order: 2
+        required: true,
+        order: 6
       })
     ];
 
